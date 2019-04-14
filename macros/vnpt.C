9a823980ac09287fe575f492039b7fad1d5a7604
Steve Sanders <ssanders@ku.edu>
2019-04-14 14:29:00 -0500

    fixed

--- macros/vnpt.C
+++ macros/vnpt.C
@@ -4,6 +4,8 @@
 #include "Framework.h"
 #include "TGraphErrors.h"
 #include "TCanvas.h"
+#include "TLatex.h"
+#include "TPaveText.h"
 #include "../CMSSW_10_3_2/src/RecoHI/HiEvtPlaneAlgos/interface/HiEvtPlaneList.h"
 using namespace hi;
 using namespace std;
@@ -15,15 +17,29 @@ static const int nptbins = 17;
 static const float ptbins[]={0.3, 0.4, 0.5,  0.6,  0.8,  1.0,  1.25,  1.50,  2.0,
 			      2.5,  3.0,  3.5,  4.0,  5.0,  6.0,  7.0, 8.0, 10.};
 
-TGraphErrors * vnpt(){
+TH1D * diffy[17];
+TH1D * diffx[17];
+
+TGraphErrors * vnpt(int mincent=5,int maxcent = 10, double etamin = 0, double etamax = 0.8){
   int order = 2;
+  for(int i = 0; i<nptbins; i++) {
+    diffx[i]=new TH1D(Form("diffx%d",i),Form("diffx%d",i),500,-1,1);
+    diffy[i]=new TH1D(Form("diffy%d",i),Form("diffy%d",i),500,-1,1);
+    diffx[i]->SetXTitle("(v_{2,x}^{obs,hfp}-v_{2,x}^{obs,hfm})/2");
+    diffx[i]->SetYTitle("Events");
+    diffy[i]->SetXTitle("(v_{2,y}^{obs,hfp}-v_{2,y}^{obs,hfm})/2");
+    diffy[i]->SetYTitle("Events");
+
+  }
   Framework * fm = new Framework();
   Framework * fp = new Framework();
+  int iroim = 0;
+  int iroip = 0;
   for(int i = 0; i<nptbins; i++) {
-    int iroim = fm->SetROIRange(order, 25, 30, -0.4, 0.0, ptbins[i],ptbins[i+1]);
+    iroim = fm->SetROIRange(order, mincent, maxcent, -etamax, -etamin, ptbins[i],ptbins[i+1]);
     if(iroim<0) return NULL;
     fm->SetROIEP(iroim,HFp2,HFm2,trackmid2);
-    int iroip = fp->SetROIRange(order, 25, 30, 0.0, 0.4, ptbins[i],ptbins[i+1]);
+    iroip = fp->SetROIRange(order, mincent, maxcent, etamin, etamax, ptbins[i],ptbins[i+1]);
     if(iroip<0) return NULL;
     fp->SetROIEP(iroip,HFm2,HFp2,trackmid2);
   }
@@ -31,23 +47,29 @@ TGraphErrors * vnpt(){
   TH1D * specp = fp->GetSpectra(0);
   int count = 0;
   int partcount = 0;
-  for(int i = 0; i<fm->GetN(); i++) {
+ rep:  for(int i = 0; i<fm->GetN(); i++) {
     signal(SIGINT,intHandler);
     if(keepRunning == 0) break;
     fm->AddEvent(i);
-    double fmx = fm->GetVnxEvt();
-    double fmy = fm->GetVnyEvt();
     fp->AddEvent(i);
-    double fpx = fp->GetVnxEvt();
-    double fpy = fp->GetVnyEvt();
-
+    for (int j = 0; j<iroim; j++){
+      double fmx = fm->GetVnxEvt(j);
+      double fmy = fm->GetVnyEvt(j);
+      double fpx = fp->GetVnxEvt(j);
+      double fpy = fp->GetVnyEvt(j);
+      if(fmx>-2&&fmy>-2&&fpx>-2&&fpy>-2) {
+	diffx[j]->Fill((fpx-fmx)/2.);
+	diffy[j]->Fill((fpy-fmy)/2.);
+      }
+    }
     ++count;
     ++partcount;
-    if(partcount == 10000) {
+    if(partcount == 50000) {
       cout<<count<<endl;
       partcount = 0;
     }
   }
+  if(fm->AddFile()&&keepRunning == 1) goto rep;
   cout<<"Processed "<<count<<" events"<<endl;
   //
   double xm[17];
@@ -121,14 +143,71 @@ TGraphErrors * vnpt(){
   specp->SetLineColor(kRed);
   specp->SetMarkerColor(kRed);
   specp->Draw("same");
-  TCanvas * c2 = new TCanvas("vn2d","vn2d",1200,1200);
+  TCanvas * c2 = new TCanvas("vnm2d","vnm2d",1200,1200);
   c2->Divide(4,4);
   for(int i = 0; i<16; i++) {
     c2->cd(i+1);
     fm->Get2d(i)->Draw();
     gPad->SetGrid(1,1);
+    TLatex * l = new TLatex(-1.2,-1.2,Form("%3.1f<p_{T}<%3.1f GeV/c",ptbins[i],ptbins[i+1]));
+    l->Draw();
+    if(i==0) {
+      TLatex * lt = new TLatex(-1.2,1.2,Form("%3.1f < #eta < %3.1f; %d - %d%c",-etamax, etamin, mincent,maxcent,'%'));
+      lt->Draw();
+    }
   }
 
+  TCanvas * c3 = new TCanvas("vnp2d","vnp2d",1200,1200);
+  c3->Divide(4,4);
+  for(int i = 0; i<16; i++) {
+    c3->cd(i+1);
+    fp->Get2d(i)->Draw();
+    gPad->SetGrid(1,1);
+    TLatex * l = new TLatex(-1.2,-1.2,Form("%3.1f<p_{T}<%3.1f GeV/c",ptbins[i],ptbins[i+1]));
+    l->Draw();
+    if(i==0) {
+      TLatex * lt = new TLatex(-1.2,1.2,Form("%3.1f < #eta < %3.1f; %d - %d%c",etamin, etamax, mincent,maxcent,'%'));
+      lt->Draw();
+    }
+  }
+
+  TCanvas * c4 = new TCanvas("diffx","diffx",1200,1200);
+  c4->Divide(4,4);
+  for(int i = 0; i<16; i++) {
+    c4->cd(i+1);
+    diffx[i]->Draw();
+    gPad->SetGrid(1,1);
+    gPad->SetLogy();
+    TPaveText * pt = new TPaveText(0.2,0.2,0.6,0.27,"NDC");
+    pt->SetBorderSize(0);
+    pt->SetFillColor(kWhite);
+    pt->AddText(Form("%3.1f<p_{T}<%3.1f GeV/c",ptbins[i],ptbins[i+1]));
+    pt->Draw();
+    TPaveText * pt2 = new TPaveText(0.2,0.85,0.6,0.93,"NDC");
+    pt2->SetBorderSize(0);
+    pt2->SetFillColor(kWhite);
+    pt2->AddText(Form("%3.1f < #eta < %3.1f; %d - %d%c",etamin, etamax, mincent,maxcent,'%'));
+    pt2->Draw();
+  }
+
+  TCanvas * c5 = new TCanvas("diffy","diffy",1200,1200);
+  c5->Divide(4,4);
+  for(int i = 0; i<16; i++) {
+    c5->cd(i+1);
+    diffy[i]->Draw();
+    gPad->SetGrid(1,1);
+    gPad->SetLogy();
+    TPaveText * pt = new TPaveText(0.2,0.2,0.6,0.27,"NDC");
+    pt->SetBorderSize(0);
+    pt->SetFillColor(kWhite);
+    pt->AddText(Form("%3.1f<p_{T}<%3.1f GeV/c",ptbins[i],ptbins[i+1]));
+    pt->Draw();
+    TPaveText * pt2 = new TPaveText(0.2,0.85,0.6,0.93,"NDC");
+    pt2->SetBorderSize(0);
+    pt2->SetFillColor(kWhite);
+    pt2->AddText(Form("%3.1f < #eta < %3.1f; %d - %d%c",etamin, etamax, mincent,maxcent,'%'));
+    pt2->Draw();
+  }
   return gm ;
 }
 
