4612ec84e6cc5e30a5132f96e97dc44a1121526a
Steve Sanders <ssanders@ku.edu>
2019-05-29 10:07:50 -0500

    up to date

--- macros/checkXY.C
+++ macros/checkXY.C
@@ -12,6 +12,7 @@
 #include "TEllipse.h"
 #include "TLine.h"
 #include "TPaveText.h"
+#include "TSystem.h"
 #include "Framework.h"
 #include "../CMSSW_10_3_2/src/RecoHI/HiEvtPlaneAlgos/interface/HiEvtPlaneList.h"
 using namespace hi;
@@ -27,12 +28,6 @@ static const float ptbins[]={0.3, 0.4, 0.5,  0.6,  0.8,  1.0,  1.25,  1.50,  2.0
 			      2.5,  3.0,  3.5,  4.0,  5.0,  6.0,  7.0, 8.0, 10.};
 static const int ncentbins = 13;
 static const int centbins[]={0,5,10,15,20,25,30,35,40,50,60,70,80,100};
-static const int chkcbin = 9;
-static const int chkptbin = 0;
-//static const int nptbins = 1;
-//static const float ptbins[]={0.3,  0.5};
-//static const int ncentbins = 1;
-//static const int centbins[]={50,60};
 static const int Order = 2;
 FILE * flist;
 TH1I * runs;
@@ -45,6 +40,15 @@ TH1D * runqdifcnt[ncentbins][nptbins];
 TH1I * hruns;
 double runlist[500];
 int nruns = 0;
+int ROIp[ncentbins][nptbins];
+int ROIm[ncentbins][nptbins];
+int mapp[ncentbins][nptbins];
+int mapm[ncentbins][nptbins];
+int centloc[500];
+int ptloc[500];
+Framework * frame;
+
+void CreatePlots(int icent, int ipt);
 
 void checkXY(string inlist="filelist.dat"){
   char buf[120];
@@ -66,19 +70,13 @@ void checkXY(string inlist="filelist.dat"){
   double vtx;
   unsigned int runno_;
   TTree * tr;
-  int ROIp[ncentbins][nptbins];
-  int ROIm[ncentbins][nptbins];
-  int mapp[ncentbins][nptbins];
-  int mapm[ncentbins][nptbins];
   //  system(Form("cat %s",inlist.data()));
-  Framework * frame = new Framework(inlist);
+  frame = new Framework(inlist);
   frame->SetMinMult(2);
   frame->SetRuns(nruns,runlist);
   if(!frame->LoadOffsets("checkXY_offsets.root")) {
     cout<<"Failed to open offsets file"<<endl; }
   
-  int centloc[500];
-  int ptloc[500];
   int iloc = 0;
   for(int i = 0; i<ncentbins; i++) {
     for(int j = 0; j<nptbins; j++) {
@@ -168,10 +166,32 @@ void checkXY(string inlist="filelist.dat"){
      ((TH2D *)frame->Get2d(i))->Write();
      ((TH1D *)frame->GetMult(i))->Write();
    }
-  TCanvas * clow = new TCanvas("clow","clow",1300,1300);
+
+
+  for(int i = 0; i<ncentbins; i++) {
+    for(int j = 0; j<nptbins; j++) {
+      subsubdirs[i][j]->cd();
+      hDiff[i][j]->Write("diff2d");
+      runqdifx[i][j]->Write("qdifx");
+      runqdify[i][j]->Write("qdify");
+      runqdifcnt[i][j]->Write("qdifcnt");
+      CreatePlots(i,j);
+    }
+  }
+  for(int i = 0; i<frame->GetNrange(); i++) {
+    frame->SaveFrameworkRuns(i,subsubdirs[centloc[i]][ptloc[i]]);
+  }
+
+}
+
+void   CreatePlots(int icent, int ipt){
+
+  string cname = Form("xy_%d_%d_%3.1f_%3.1f",centbins[icent],centbins[icent+1],ptbins[ipt],ptbins[ipt+1]);
+  TCanvas * clow = new TCanvas(cname.data(),cname.data(),1300,1300);
   clow->Divide(2,2);
   clow->cd(1);
-  frame->Get2d(mapm[chkcbin][chkptbin])->Draw();
+
+  frame->Get2d(mapm[icent][ipt])->Draw();
   gPad->SetGrid(1,1);
   TLine * l1a = new TLine(-1.4,0,1.4,0);
   l1a->SetLineWidth(2);
@@ -186,7 +206,7 @@ void checkXY(string inlist="filelist.dat"){
   el1a->SetLineColor(kRed);
   el1a->Draw();
   clow->cd(2);
-  frame->Get2d(mapp[chkcbin][chkptbin])->Draw();
+  frame->Get2d(mapp[icent][ipt])->Draw();
   gPad->SetGrid(1,1);
   TLine * l1b = new TLine(-1.4,0,1.4,0);
   l1b->SetLineWidth(2);
@@ -201,7 +221,7 @@ void checkXY(string inlist="filelist.dat"){
   el1b->SetLineColor(kRed);
   el1b->Draw();
   clow->cd(3);
-  hDiff[chkcbin][chkptbin]->Draw();
+  hDiff[icent][ipt]->Draw();
   gPad->SetGrid(1,1);
   TLine * l1c = new TLine(-1.4,0,1.4,0);
   l1c->SetLineWidth(2);
@@ -214,56 +234,13 @@ void checkXY(string inlist="filelist.dat"){
   tp->SetBorderSize(0);
   tp->SetFillColor(kWhite);
   tp->AddText("2018 PbPb");
-  tp->AddText(Form("%d - %d%c",centbins[chkcbin],centbins[chkcbin+1],'%'));
-  tp->AddText(Form("%3.1f < p_{T} <  %3.1f GeV/c",ptbins[chkptbin],ptbins[chkptbin+1]));
+  tp->AddText(Form("%d - %d%c",centbins[icent],centbins[icent+1],'%'));
+  tp->AddText(Form("%3.1f < p_{T} <  %3.1f GeV/c",ptbins[ipt],ptbins[ipt+1]));
   tp->Draw();
-  clow->Print("chckXY.pdf","pdf");
-
-  // TCanvas * chigh = new TCanvas("chigh","chigh",1300,680);
-  // chigh->Divide(2);
-  // chigh->cd(1);
-  // frame->Get2d(mapm[12][3])->Draw();
-  // gPad->SetGrid(1,1);
-  // TLine * l3a = new TLine(-1.4,0,1.4,0);
-  // l3a->SetLineWidth(2);
-  // l3a->Draw();
-  // TLine * l4a = new TLine(0,-1.4,0,1.4);
-  // l4a->SetLineWidth(2);
-  // l4a->Draw();
-  // TEllipse * el2a = new TEllipse(0,0,1,1);
-  // el2a->SetFillStyle(0);
-  // el2a->SetLineWidth(3);
-  // el2a->SetLineStyle(2);
-  // el2a->SetLineColor(kRed);
-  // el2a->Draw();
-  // chigh->cd(2);
-  // frame->Get2d(mapp[12][3])->Draw();
-  // chigh->SetGrid(1,1);
-  // gPad->SetGrid(1,1);
-  // TLine * l3b = new TLine(-1.4,0,1.4,0);
-  // l3b->SetLineWidth(2);
-  // l3b->Draw();
-  // TLine * l4b = new TLine(0,-1.4,0,1.4);
-  // l4b->SetLineWidth(2);
-  // l4b->Draw();
-  // TEllipse * el2b = new TEllipse(0,0,1,1);
-  // el2b->SetFillStyle(0);
-  // el2b->SetLineWidth(3);
-  // el2b->SetLineStyle(2);
-  // el2b->SetLineColor(kRed);
-  // el2b->Draw();
-
-  for(int i = 0; i<ncentbins; i++) {
-    for(int j = 0; j<nptbins; j++) {
-      subsubdirs[i][j]->cd();
-      hDiff[i][j]->Write("diff2d");
-      runqdifx[i][j]->Write("qdifx");
-      runqdify[i][j]->Write("qdify");
-      runqdifcnt[i][j]->Write("qdifcnt");
-    }
-  }
-  for(int i = 0; i<frame->GetNrange(); i++) {
-    frame->SaveFrameworkRuns(i,subsubdirs[centloc[i]][ptloc[i]]);
-  }
-
+  clow->Print(Form("figures/checkXY/%s.pdf",clow->GetName()),"pdf");
+  clow->Close();
+  gSystem->ProcessEvents();
+  delete clow;
+  clow = 0;
+  return;
 }
