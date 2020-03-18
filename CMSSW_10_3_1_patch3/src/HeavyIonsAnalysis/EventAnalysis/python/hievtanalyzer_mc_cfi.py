d2b686ac1ae20caf1da0e1a2c6c44311fce33ea0
Steve Sanders <ssanders@ku.edu>
2020-03-18 10:39:14 -0500

    changes

--- CMSSW_10_3_1_patch3/src/HeavyIonsAnalysis/EventAnalysis/python/hievtanalyzer_mc_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-from HeavyIonsAnalysis.EventAnalysis.hievtanalyzer_data_cfi import *
-
-hiEvtAnalyzer.doMC   = True
-hiEvtAnalyzer.doHiMC = True
--- CMSSW_10_3_1_patch3/src/HeavyIonsAnalysis/EventAnalysis/python/skimanalysis_cfi.py
+++
@@ -1,6 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-skimanalysis = cms.EDAnalyzer("FilterAnalyzer",
-    hltresults = cms.InputTag("TriggerResults","","HiForest"),
-    superFilters = cms.vstring("")
-)
